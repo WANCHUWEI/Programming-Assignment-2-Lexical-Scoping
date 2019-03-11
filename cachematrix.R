c4358cf5161c9f4af1c691c1a5ebc1cddfcf2212
WANCHUWEI <48449909+WANCHUWEI@users.noreply.github.com>
2019-03-11 13:58:43 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##set the value of the vector
+##get the value of the vector
+##set the value of the mean
+##get the value of the mean
 
+makeCacheMatrix <- function(x = matrix()) {
+        J <- NULL
+        set <- function(y) {
+                x <<- y
+                J <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) J <<- mean
+        getmean <- function() J
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
 ## Write a short comment describing this function
+##The following function calculates the mean of the special "vector" created with the makeCacheMatrix.
+##Firstly, checking if the mean has already been calculated.
+##If the mean has already been calculated, it should get the mean from the cache and skip the computation.
+##If the mean has not been calculated, the mean needs to be calculated from the cache. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        J <- x$getmean()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(J)
+        }
+        data <- x$get()
+        J <- mean(data, ...)
+        x$setmean(J)
+        J
 }
