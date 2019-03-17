3647140f2de15a30f5d0f4e983e41657bbade9bf
WANCHUWEI <48449909+WANCHUWEI@users.noreply.github.com>
2019-03-17 15:06:32 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(M = matrix()) {
         J <- NULL
         ##set the matrix
         set <- function(matrix) {
