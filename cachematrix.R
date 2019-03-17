60a373bdcdca0767938a927990d4cb2f71cf94c4
WANCHUWEI <48449909+WANCHUWEI@users.noreply.github.com>
2019-03-17 15:00:04 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,40 +3,42 @@
 
 ## Write a short comment describing this function
 
-##set the value of the vector
-##get the value of the vector
-##set the value of the mean
-##get the value of the mean
-
 makeCacheMatrix <- function(x = matrix()) {
         J <- NULL
-        set <- function(y) {
-                x <<- y
+        ##set the matrix
+        set <- function(matrix) {
+                M <<- matrix
                 J <<- NULL
         }
-        get <- function() x
-        setmean <- function(mean) J <<- mean
-        getmean <- function() J
+        ##get the matrix
+        get <- function() M
+        ##set the inverse of the matrix
+        setinverse <- function(inverse) J <<- inverse
+        ##get the inverse of the matrix
+        getinverse <- function() J
+        ## Return a list
         list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 ##The following function calculates the mean of the special "vector" created with the makeCacheMatrix.
-##Firstly, checking if the mean has already been calculated.
-##If the mean has already been calculated, it should get the mean from the cache and skip the computation.
-##If the mean has not been calculated, the mean needs to be calculated from the cache. 
 
 cacheSolve <- function(x, ...) {
-        J <- x$getmean()
-        if (!is.null(inv)) {
+        ## Return a matrix that is the inverse of 'x'
+        M <- x$getinverse()
+        ##return the inverse
+        if (!is.null(M)) {
                 message("getting cached data")
-                return(J)
+                return(M)
         }
         data <- x$get()
-        J <- mean(data, ...)
-        x$setmean(J)
-        J
+        ##Calculate the inverse by using matrix
+        M <- solve(data) %*% data
+        ## Set the inverse to the object
+        x$setinverse(M)
+        ## Return the matrix
+        M
 }
